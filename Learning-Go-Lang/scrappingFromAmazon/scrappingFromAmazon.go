6b0fa8a9cbcc17e3d427081aeee44fa32fa5368e
Arifa Akter <arifa.a.asha@gmail.com>
2022-10-18 13:14:06 +0600

    Scrapping from amzon plushies and saved in a slice

--- Learning-Go-Lang/scrappingFromAmazon/scrappingFromAmazon.go
+++ Learning-Go-Lang/scrappingFromAmazon/scrappingFromAmazon.go
@@ -20,23 +20,23 @@ func main() {
 		fmt.Println("Link of the page:", r.URL)
 	})
 
+	var items []item //blank list or black slice
+
 	c.OnHTML("div.s-result-list.s-search-results.sg-row", func(h *colly.HTMLElement) {
 		h.ForEach("div.a-section.a-spacing-base", func(_ int, h *colly.HTMLElement) {
-			var name string
-			name = h.ChildText("span.a-size-base-plus.a-color-base.a-text-normal")
-
-			var imgURL string
-			imgURL = h.ChildAttr("img", "src")
-
-			var price string
-			price = h.ChildText("span.a-price-whole")
-
-			fmt.Println("ProductName: ", name)
-			fmt.Println("ImgURL: ", imgURL)
-			fmt.Println("Price: ", price)
-
+			item := item{
+				Name:   h.ChildText("span.a-size-base-plus.a-color-base.a-text-normal"),
+				Price:  h.ChildText("span.a-price-whole"), //childext => child element
+				ImgUrl: h.ChildAttr("img", "src"),
+			}
+			items = append(items, item)
 		})
 	})
 
 	c.Visit("https://www.amazon.com/s?k=plushies")
+
+	for _, item := range items {
+		fmt.Printf("%s\n-----------\n", item)
+	}
+
 }
