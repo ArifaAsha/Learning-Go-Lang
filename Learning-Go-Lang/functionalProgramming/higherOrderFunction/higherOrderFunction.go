083f9723e11a7593d014864df85536d1321034dd
Arifa Akter <arifa.a.asha@gmail.com>
2022-10-26 12:24:20 +0600

    Closure exmple is added in higherorderFunction file

--- Learning-Go-Lang/functionalProgramming/higherOrderFunction/higherOrderFunction.go
+++ Learning-Go-Lang/functionalProgramming/higherOrderFunction/higherOrderFunction.go
@@ -10,6 +10,18 @@ func printNumber(number string) {
 	fmt.Println("The number is: ", number)
 }
 
+// Closure ->
+func getClicker() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
 func main() {
 	callPrintNumber(printNumber, "12222")
+	click := getClicker()
+	fmt.Println(click())
+	fmt.Println(click())
 }
